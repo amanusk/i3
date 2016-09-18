ba8b37797b9e8a5fcf766d1dc8db07542550e2a9
amanusk <alex.manuskin@gmail.com>
2016-09-18 13:29:31 +0300

    Alt tab

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-## i3
+# i3
 
 This will explain what stems you need to have i3 setup theway you want and install all the necessary features
 
-# install pasytray for volume control
+## required installations
 ```
 sudo apt install pasystray &&\
 sudo apt install gnome-tweak-tool &&\
 sudo apt install i3blocks &&\
+sudo apt install ranger &&\
+sudo apt install cmus &&\
+sudo apt install highlight &&\
+sudo apt install scrot &&\
+mkdir -p ~/Pictures/Scrot &&\
+```
+
+## install playerctl controll (ubuntu)
+```
+git clone https://github.com/acrisci/playerctl.git && \
+cd playerctl && \
+sudo apt-get install gtk-doc-tools gobject-introspection build-essential libglib2.0-dev && \
+./autogen.sh && \
+make && \
+sudo make install && \
+sudo ldconfig
 ```
--- display.sh
+++ display.sh
@@ -11,6 +11,32 @@ else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
+# Degect extenal monitor
+if [ `xrandr | grep VGA1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="VGA13"
+fi
+if [ `xrandr | grep DVI1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DVI1"
+fi
+if [ `xrandr | grep HDMI1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI1"
+fi
+if [ `xrandr | grep HDMI2 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI2"
+fi
+if [ `xrandr | grep HDMI3 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI3"
+fi
+if [ `xrandr | grep DP1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DP1"
+fi
+if [ `xrandr | grep DP2 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DP2"
+fi
+if [ `xrandr | grep DP3 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DP2"
+fi
+
 if [ $monitor_mode = "all" ]; then
         monitor_mode="EXTERNAL"
         xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
