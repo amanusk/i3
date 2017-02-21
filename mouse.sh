27065e96a6492711bc8fa9e6632e5dca939b7bac
amanusk <alex.manuskin@gmail.com>
2017-02-21 20:37:07 +0200

    Update mouse and display

--- mouse.sh
+++ mouse.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 input=`xinput list | grep Lite-On | grep pointer | cut -d "=" -f 2 | cut -d$'\t' -f 1`
-xinput set-prop $input 262 0.3
+prop=`xinput list-props $input | grep "Constant Deceleration" | cut -d"(" -f 2 | cut -d")" -f1`
+xinput set-prop $input $prop 0.3
+
+# To list properties:
+# xinput list-props $input
