ccf6374111311c86b8eefd095948480b44202dbb
amanusk <alex.manuskin@gmail.com>
2017-10-04 10:47:53 +0300

    Add multi-display

--- multi-display.sh
+++ multi-display.sh
@@ -3,3 +3,5 @@
 xrandr --output LVDS-1 --off \
     --output HDMI-3 --auto --primary \
     --output VGA-1 --auto --right-of HDMI-3
+
+setxkbmap -model pc105 -layout us,il -option grp:alt_shift_toggle
--- single-display.sh
+++ single-display.sh
@@ -4,3 +4,4 @@ xrandr --output HDMI-3 --off \
     --output VGA-1 --off \
     --output LVDS-1 --auto --primary
 
+setxkbmap -model pc105 -layout us,il -option grp:alt_shift_toggle
