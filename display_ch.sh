28eee7d6f910ba1533c863b8c1bc25617d3e0137
Alex Manuskin <amanusk@tuta.io>
2019-12-28 14:22:10 +0200

    Update for debian

--- display_ch.sh
+++ display_ch.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
 
 
-INTERNAL_OUTPUT="LVDS-1"
+INTERNAL_OUTPUT="LVDS1"
 
-choices="laptop\ndual\nexternal\ncolne\ntechnion"
+choices="laptop\ndual\nexternal\nclone"
 
 chosen=$(echo -e $choices | dmenu -i)
 
 
 # Detect extenal monitor
-if [ `xrandr | grep VGA-1 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="VGA-1"
+if [ `xrandr | grep VGA1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="VGA1"
 fi
-if [ `xrandr | grep DVI-1 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="DVI-1"
+if [ `xrandr | grep DVI1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DVI1"
 fi
-if [ `xrandr | grep HDMI-1 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="HDMI-1"
+if [ `xrandr | grep HDMI1 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI1"
 fi
-if [ `xrandr | grep HDMI-2 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="HDMI-2"
+if [ `xrandr | grep HDMI2 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI2"
 fi
-if [ `xrandr | grep HDMI-3 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="HDMI-3"
+if [ `xrandr | grep HDMI-3 | grep c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="HDMI3"
 fi
 if [ `xrandr | grep DP1 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="DP-1"
+	EXTERNAL_OUTPUT="DP1"
 fi
-if [ `xrandr | grep DP-2 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="DP-2"
+if [ `xrandr | grep DP2 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DP2"
 fi
-if [ `xrandr | grep DP-3 | grep -c ' connected '` -eq 1 ]; then
-	EXTERNAL_OUTPUT="DP-2"
+if [ `xrandr | grep DP3 | grep -c ' connected '` -eq 1 ]; then
+	EXTERNAL_OUTPUT="DP2"
 fi
 
 
 case "$chosen" in
-    external) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary ;;
-    laptop) xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
-    clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT ;;
-    dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary ;;
-    technion) xrandr --output LVDS-1 --off --output HDMI-3 --auto --primary && sleep 1 && xrandr --output HDMI-2 --auto --right-of HDMI-3 ;;
+    external) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary && ~/.fehbg;;
+    laptop) xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off && ~/.fehbg;;
+    clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT && ~/.fehbg;;
+    dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary && ~/.fehbg;;
 
 esac
 
